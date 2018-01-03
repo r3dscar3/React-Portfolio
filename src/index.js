58c62fa1645cf99f47470cc93b98c6423f2f890d
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-03 10:20:18 -0500

    Fixing Build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route }
+import { BrowserRouter, Route } from 'react-router-dom';
 import * as layouts from './views/layouts';
 import registerServiceWorker from './registerServiceWorker';
 
