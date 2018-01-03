fa5e99f4c0199572926ba0fa42c3eb6d2f5a5b7d
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-02 22:15:39 -0500

    Adding router

---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+	render() {
+		return (
+			<div className="App">
+	    	<h1>Nolan Thompson</h1>
+	      <p className="App-intro">
+	      	This is the start of something...
+	      </p>
+	    </div>
+		)	
+	}
+}
+
+export default Home;
