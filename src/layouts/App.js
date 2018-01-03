534b0baec5ae948a2d5cde909538e02dd1302a24
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-02 22:32:41 -0500

    Adding Router

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
+import pages from '../pages';
+
 import '../styles/App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Nolan Panther</h1>
-        </header>
-        <p className="App-intro">
-          This is the start of something...
-        </p>
-      </div>
+      <Router>
+      	<Route path={'/'} component={pages.Home} />
+      	<Route path={'/test'} component={pages.Test} />
+      </Router>
     );
   }
 }
