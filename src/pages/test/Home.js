d52831e2c7e50aa957588415c5973ef34f4f2d5e
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-02 22:38:38 -0500

    Test Files for testing routes

---
+++ src/pages/test/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+	render() {
+		return (
+			<div className="App">
+	    	<h1>Testies</h1>
+	      <p className="App-intro">
+	      	123
+	      </p>
+	    </div>
+		)	
+	}
+}
+
+export default Test;
