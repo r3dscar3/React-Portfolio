b4937297f3178c0aa7a4acf4435ee6d4983d4174
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-02 22:46:38 -0500

    Adding Route to fix build

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import pages from '../pages';
 
 import '../styles/App.scss';
