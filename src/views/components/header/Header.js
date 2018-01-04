fe85cc8d3b3a6eb19a92edb83f5fc029cf3c096f
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-03 21:06:48 -0500

    Styling changes

--- src/views/components/header/Header.js
+++ src/views/components/header/Header.js
@@ -7,7 +7,7 @@ import Logo from '../../../images/logo.svg';
 const Header = () => {
 	return (
 		<div className="header">
-			<Link to={'/'}><img src={Logo} alt="Glasses" /></Link>
+			<Link to={'/'}><img className="logo" src={Logo} alt="Glasses" /></Link>
 			<ul className="nav">
 				<li>
 					<Link to={'/about'}>About Me</Link>
