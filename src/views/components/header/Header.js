66d47d75c42fc66e5887aab3d996e4cc03af3536
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-03 20:16:03 -0500

    Fixing build

--- src/views/components/header/Header.js
+++ src/views/components/header/Header.js
@@ -1,7 +1,7 @@
 import './styles.scss';
 
 import React from 'react';
-import { Link } from 'react-router-dom':
+import { Link } from 'react-router-dom';
 import Logo from '../../../images/logo.svg';
 
 const Header = () => {
