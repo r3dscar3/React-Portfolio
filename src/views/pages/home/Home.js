7188c2ee813083bbb12991f8e2a80968266d7db8
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-04 17:58:11 -0500

    home page changes

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,9 +1,12 @@
+import './styles.scss';
+
 import React, {Component} from 'react';
 
 class Home extends Component {
 	render() {
 		return (
 			<div className="home-page">
+	      <h2>Hello</h2>
 	      <p className="home-intro">
 	      	This is the start of something...
 	      </p>
