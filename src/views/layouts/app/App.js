e0f825a3ac39fedb9d137f1cd315bff2caa1b1bd
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-05 21:04:31 -0500

    testing debugger

--- src/views/layouts/app/App.js
+++ src/views/layouts/app/App.js
@@ -7,21 +7,42 @@ import Header from '../../components/header';
 import Footer from '../../components/footer';
 
 class App extends Component {
+	
+	if (!console) {
+		console = {};
+	}
+	
+	let old = console.log;
+	let logger = document.getElementById('console');
+	
+	console.log = (message) => {
+		if (typeof message == 'object') {
+	  	logger.innerHTML += (JSON && JSON.stringify
+	    	? JSON.stringify(message)
+	    	: String(message)) + '<br />';
+	  } else {
+	  	logger.innerHTML += message + '<br />';
+	  }
+	}
+    
   render() {
     return (
-      <div id="wrapper">
-      	<header>
-      		<Header />   
-      	</header>
-      	<div className="main-section">
-      		<main className="content">
-			      	<Route exact path={'/'} component={pages.Home} />
-      			<Route path={'/test'} component={pages.Test} />
-      		</main>
-      	</div>
-      	<footer>
-      		<Footer />
-      	</footer>
+    	<div className="debug-wrapper">
+	      <div id="wrapper">
+	      	<header>
+	      		<Header />   
+	      	</header>
+	      	<div className="main-section">
+	      		<main className="content">
+				      	<Route exact path={'/'} component={pages.Home} />
+	      			<Route path={'/test'} component={pages.Test} />
+	      		</main>
+	      	</div>
+	      	<footer>
+	      		<Footer />
+	      	</footer>
+	      </div>
+	      <div id="console" />
       </div>
     );
   }
