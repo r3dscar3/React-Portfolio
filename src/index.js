c3011c456020ec1da9d1c8fb9ba1f1b14c08fdea
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-06 13:46:14 -0500

    debug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import * as layouts from './views/layouts';
 import registerServiceWorker from './registerServiceWorker';
-import './debug/console-log-div.js';
+
 render(
 	<BrowserRouter basename={'/'}>
 		<Route path="/" component={layouts.App} />
