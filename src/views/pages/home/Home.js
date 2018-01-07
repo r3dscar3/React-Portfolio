c00edb90c0156e1a73999a03086b42606183c47c
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-07 17:40:01 -0500

    home page

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,15 +1,47 @@
 import './styles.scss';
 
 import React, {Component} from 'react';
+import MainSet from '../../../images/logo.svg';
+import TouchPad from '../../../images/logo.svg';
+import TightLine from '../../../images/logo.svg';
+
+const projects = [
+	[
+		{
+			title: "MainSet",
+			description: "HubSpot CMS, jquery, bootstrap",
+			img: MainSet
+		},
+		{
+			title: "TouchPad Live",
+			description: "Angular, jquery, bootstrap",
+			img: MainSet
+		},
+		{
+			title: "TightLine Adventures",
+			description: "WordPress, jquery, bootstrap",
+			img: TightLine
+		}
+	]
+]
 
 class Home extends Component {
 	render() {
 		return (
 			<div className="home-page">
-	      <h2>Hello</h2>
-	      <p className="home-intro">
-	      	This is the start of something...
-	      </p>
+	      {
+	      	Object.keys(projects).map(project => {
+	      		<div key={project.title} className="project">
+	      			<img src={project.img} />
+	      			<h2 className="project-title">
+	      				{project.title}
+	      			</h2>
+	      			<p className="project-desc">
+	      				{project.description}
+	      			</p>
+	      		</div>
+	      	})
+	      }
 	    </div>
 		)	
 	}
