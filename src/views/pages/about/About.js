be50608b0850f499b0d4c15c9f543cfc7c37236e
Nolan Thompson <r3dscar3@users.noreply.github.com>
2018-01-13 10:22:18 -0500

    About page

---
+++ src/views/pages/about/About.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+	render() {
+		return (
+			<div className="about-page">
+	      <p>
+	      	Frontend web and application developer working in NYC for the National Hockey League. This is my journey up until now.
+	      </p>
+	    </div>
+		)	
+	}
+}
+
+export default About;
