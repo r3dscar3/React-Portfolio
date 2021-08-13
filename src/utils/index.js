b68f476e9591d9cd6b4f1ec578c5bb1cdbea4129
Nolan Thompson <nolan.thompson@walmart.com>
2021-08-12 23:33:35 -0700

    fixng some more api stuff

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 export const splitLineBreaks = (str) => str.split(new RegExp('\\r?\\n'));
 
-export const sortAsc = (arr) => arr && arr.sortOrder((a, b) => a.order - b.order);
+export const sortAsc = (arr) => arr && arr.sort((a, b) => a.sortOrder - b.sortOrder);
