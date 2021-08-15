32675a307731d84ae53c38b427339449e6120f23
Nolan Thompson <nolan.thompson@walmart.com>
2021-08-15 16:05:54 -0700

    setting overflow hidden on html and body

--- src/components/layout/GlobalStyle.js
+++ src/components/layout/GlobalStyle.js
@@ -146,8 +146,9 @@ const GlobalStyle = () => {
         body,
         html,
         #__next {
-          height: 100%;
-          width: 100%;
+          height: 100vh;
+          width: 100vw;
+          overflow: hidden;
         }
 
         body {
