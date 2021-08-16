175f051fd8d089de814f3bdaea0fb4662be36c1b
Nolan Thompson <nolan.thompson@walmart.com>
2021-08-16 07:35:02 -0700

    ios gutter fix

--- src/components/layout/GlobalStyle.js
+++ src/components/layout/GlobalStyle.js
@@ -154,7 +154,11 @@ const GlobalStyle = () => {
         body {
           color: #4a4a4a;
           font-family: ${theme.fontStack};
-          background-color: ${theme.colors.backgroundContent};
+          background-color: linear-gradient(
+            to right,
+            ${theme.colors.backgroundMenu} 50px,
+            white 50px
+          ); //this is for ios in landscape so menu background matches in the gutter
         }
 
         * {
